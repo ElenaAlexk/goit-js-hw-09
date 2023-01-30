14c4d86921e38fa2ecea8e08dba10a06b78a4543
Elena Kvasko <lenakvasko@gmail.com>
2023-01-30 22:15:34 +0200

    Update color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+console.log('hello');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtn);
+function onStartBtn() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,9 @@
-function createPromise(position, delay) {
+/*function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
-}
+} */
+console.log('hello');
