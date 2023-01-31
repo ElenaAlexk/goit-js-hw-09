7a997c7228a7cb221d3219e0fb01378a2c08ccf1
Elena Kvasko <lenakvasko@gmail.com>
2023-01-31 09:27:06 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,18 @@
-console.log('hello');
+console.log('hellooo');
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timerId = null;
 
-startBtn.addEventListener('click', onStartBtn);
-function onStartBtn() {
+startBtn.addEventListener('click', onStartBtnClick);
+function onStartBtnClick() {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+}
+stopBtn.addEventListener('click', onStopBtnClick);
+function onStopBtnClick() {
+  clearInterval(timerId);
 }
 
 function getRandomHexColor() {
