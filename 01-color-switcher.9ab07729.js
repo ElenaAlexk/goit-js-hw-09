a1081c7c5d608afa9bbfa62b3f073e6b057e5e81
ElenaAlexk <111981718+ElenaAlexk@users.noreply.github.com>
2023-01-31 07:29:11 +0000

    Deploying to gh-pages from @ ElenaAlexk/goit-js-hw-09@0c5fd740f1610568651c2116e27e5cb9ce66354e 🚀

--- 01-color-switcher.9ab07729.js
+++
@@ -1,2 +0,0 @@
-console.log("hello");const t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));let e=null;t.addEventListener("click",(function(){e=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));
-//# sourceMappingURL=01-color-switcher.9ab07729.js.map
--- 01-color-switcher.fe20df58.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log("hello");var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.fe20df58.js.map
