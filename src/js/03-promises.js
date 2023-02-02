8d53096b62aa87adccd67f4d4df4aee33476cf91
Elena Kvasko <lenakvasko@gmail.com>
2023-02-02 21:52:50 +0200

    Update promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('click', onPromiseCreate);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+function onPromiseCreate(e) {
+  e.preventDefault();
+  let amount = Number(refs.amount.value);
+  let delayFirst = Number(refs.delay.value);
+  let step = Number(refs.step.value);
+
+  for (let i = 1; i <= amount; i = +1) {
+    let newDelay = delayFirst + step * i;
+
+    createPromise(i, newDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
